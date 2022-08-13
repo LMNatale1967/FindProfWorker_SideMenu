5b1a58f41fff10419f453478f60f4f4c285ae9f6
LMNatale1967 <LuisMiguelNatale@gmail.comm>
2022-08-13 14:32:32 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FindProfWorker_SideMenu',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
